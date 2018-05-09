237cc84c88dbcdab8ff8c37659bc0d5eba24df52
meisanggou <zhou5315938@163.com>
2018-05-09 11:29:28 +0800

    fix bug

--- web01/static/t_js/exam/add.js
+++ web01/static/t_js/exam/add.js
@@ -52,7 +52,7 @@ function new_exam(){
                 if (isConfirm){
                     var j_url = AddUrlArg(location.pathname, "exam_no", data["exam_no"]);
                     j_url = AddUrlArg(j_url, "exam_type", data["exam_type"]);
-                    location.href = AddUrlArg(j_url)
+                    location.href = j_url;
                 }
             }
         );
