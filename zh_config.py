5a57ae3e2809b2de94e5a346ceb04c79a259911b
meisanggou <zhou5315938@163.com>
2018-05-10 15:22:46 +0800

    update folder

--- zh_config.py
+++ zh_config.py
@@ -16,4 +16,4 @@ static_prefix_url = "/static01"
 db_conf_path = os.path.join(script_dir, "mysql_app.conf")
 
 # 目录相关配置
-upload_folder = os.environ.get("ZH_UPLOAD_FOLDER", "D:\\upload")
+upload_folder = os.environ.get("ZH_UPLOAD_FOLDER", "G:\\zhupload")
