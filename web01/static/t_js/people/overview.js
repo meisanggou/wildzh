27bbc91567697090f88586d5afcd89956aca0122
meisanggou <zhou5315938@163.com>
2018-06-12 19:59:03 +0800

    to vue

--- web01/static/t_js/people/overview.js
+++ web01/static/t_js/people/overview.js
@@ -2,64 +2,6 @@
  * Created by meisa on 2018/5/9.
  */
 
-function delete_doctor() {
-    var current_td = $(this).parent();
-    var current_tr = current_td.parent();
-    var tr_id = current_tr.attr("id");
-    var doctor_name = current_tr.find("td:eq(0)").text();
-    var degree = current_tr.find("td:eq(1)").text();
-    var msg = "确定要删除【" + degree + "】【" + doctor_name + "】";
-    swal({
-            title: "删除警告",
-            text: msg,
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: '#DD6B55',
-            confirmButtonText: '删除',
-            cancelButtonText: "取消",
-            closeOnConfirm: true,
-            closeOnCancel: true
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                var r_d = {"people_no": tr_id};
-                my_async_request2($("#info_url").val(), "DELETE", r_d, function (data) {
-                    location.reload();
-                });
-            }
-        }
-    );
-}
-
-function online_doctor() {
-    var current_td = $(this).parent();
-    var current_tr = current_td.parent();
-    var tr_id = current_tr.attr("id");
-    var doctor_name = current_tr.find("td:eq(0)").text();
-    var degree = current_tr.find("td:eq(1)").text();
-    var msg = "确定上线医生【" + doctor_name + "】\n上线后将不可更改信息！";
-    swal({
-            title: "上线提醒",
-            text: msg,
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: '#DD6B55',
-            confirmButtonText: '上线',
-            cancelButtonText: "取消",
-            closeOnConfirm: true,
-            closeOnCancel: true
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                var r_d = {"people_no": tr_id};
-                my_async_request2($("#online_url").val(), "POST", r_d, function (data) {
-                    location.reload();
-                });
-            }
-        }
-    );
-}
-
 function explain_status(s) {
     if ((s & 128) != 0) {
         return "已下线"
@@ -73,56 +15,81 @@ function explain_status(s) {
     return "待上线"
 }
 
-function init_info(data) {
-    if (data == null) {
-        var info_url = $("#info_url").val();
-        var args = {};
-        var group_id = UrlArgsValue(location.search, "group_id");
-        if(group_id != null){
-            args["group_id"] = group_id;
-        }
-        my_async_request2(info_url, "GET", args, init_info);
-        return 0;
-    }
-    if (data.length > 0) {
-        var keys = ["people_name", "degree", "company", "department"];
-        var t = $("#t_doctors");
-        for (var i = 0; i < data.length; i++) {
-            var add_tr = $("<tr></tr>");
-            add_tr.attr("id", data[i]["people_no"]);
+$(function () {
+    g_p_vm = new Vue({
+        el: "#div_overview",
+        data: {
+            group_id: "",
+            p_items: []
+        },
+        methods: {
+            look_p: function(index){
 
-            for (var j = 0; j < keys.length; j++) {
-                var td_t = new_td(keys[j], data[i]);
-                add_tr.append(td_t);
+            },
+            delete_p: function(index){
+                var p_item = this.p_items[index];
+                var name = p_item.people_name;
+                var degree = p_item.degree;
+                var msg = "确定要删除【" + degree + "】【" + name + "】";
+                swal({
+                        title: "删除警告",
+                        text: msg,
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: '#DD6B55',
+                        confirmButtonText: '删除',
+                        cancelButtonText: "取消",
+                        closeOnConfirm: true,
+                        closeOnCancel: true
+                    },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            var r_d = {"people_no": p_item.people_no};
+                            my_async_request2($("#info_url").val(), "DELETE", r_d, function (data) {
+                                location.reload();
+                            });
+                        }
+                    }
+                );
+            },
+            online_p: function(index){
+                var p_item = this.p_items[index];
+                var name = p_item.people_name;
+                var msg = "确定上线【" + name + "】\n上线后将不可更改信息！";
+                swal({
+                        title: "上线提醒",
+                        text: msg,
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: '#DD6B55',
+                        confirmButtonText: '上线',
+                        cancelButtonText: "取消",
+                        closeOnConfirm: true,
+                        closeOnCancel: true
+                    },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            var r_d = {"people_no": p_item.people_no};
+                            my_async_request2($("#online_url").val(), "POST", r_d, function (data) {
+                                location.reload();
+                            });
+                        }
+                    }
+                );
             }
+        }
+    });
+    var info_url = $("#info_url").val();
+    my_async_request2(info_url, "GET", null, function(data){
 
-            var td_status = new_td("status", data[i], null, null, explain_status);
-            add_tr.append(td_status);
-
-            var td_op = $("<td></td>");
+        for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            item.cn_status = explain_status(item.status);
             var basic_url = AddUrlArg(location.pathname, "people_no", data[i]["people_no"]);
             var detail_url = AddUrlArg(basic_url, "action", "update");
-            td_op.append(new_link("查看", detail_url));
-
-            td_op.append(" | ");
-            var del_link = $("<a href='javascript:void(0)'>删除</a>");
-            del_link.click(delete_doctor);
-            td_op.append(del_link);
-
-            if (data[i]["status"] == 3) {
-                var data_item = data[i];
-                td_op.append(" | ");
-                var online_link = $("<a href='javascript:void(0)'>上线</a>");
-                online_link.click(online_doctor);
-                td_op.append(online_link);
-            }
-            add_tr.append(td_op);
-
-            t.append(add_tr);
+            item.detail_url = detail_url;
+            g_p_vm.p_items.push(item);
         }
-    }
-}
-
-$(function () {
-    init_info(null);
+    });
+    //init_info(null);
 });
