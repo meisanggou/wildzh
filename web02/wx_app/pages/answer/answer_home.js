cf40f0e65e75397d2de3dc927c5551f436ba1167
zhouhenglc <zhouhenglc@inspur.com>
2019-09-24 07:59:33 +0800

    add answer home

---
+++ web02/wx_app/pages/answer/answer_home.js
@@ -0,0 +1,66 @@
+var app = getApp();
+
+Page({
+
+    data: {
+        training_modes: ['分科练习', '综合练习'],
+        subjects_array: ['微观经济学', '宏观经济学', '政治经济学'],
+        select_modes: ['选择题', '名词解释', '简答题', '计算题', '论述题'],
+        index: 1,
+        subjectIndex: 0,
+        modeIndex: 0,
+        isAccordingToType: true,
+        to: "training",
+        cacheSelectedKey: "selectedAnswerOptions"
+    },
+    onLoad: function(options) {
+        if ("to" in options) {
+            this.setData({
+                to: options["to"]
+            })
+        }
+        var selectedOptions = app.getOrSetCacheData(this.data.cacheSelectedKey);
+        if(selectedOptions != null){
+            this.setData(selectedOptions);
+        }
+    },
+    bindPickerChange(e) {
+        this.setData({
+            index: parseInt(e.detail.value)
+        })
+    },
+    subjectChange(e) {
+        this.setData({
+            subjectIndex: parseInt(e.detail.value)
+        })
+    },
+    switchModeChange(e) {
+        this.setData({
+            isAccordingToType: e.detail.value
+        })
+    },
+    selectModeChange(e) {
+        this.setData({
+            modeIndex: parseInt(e.detail.value)
+        })
+    },
+    startTraining() {
+        app.getOrSetCacheData(this.data.cacheSelectedKey, this.data);
+        var url = "";
+        if (this.data.to == "answer") {
+            url += "../answer/answer"
+        } else {
+            url += "answer"
+        }
+        url += "?from=answer_home"
+        if (this.data.isAccordingToType) {
+            url += "&select_mode=" + (this.data.modeIndex + 1);
+        }
+        if (this.data.index == 0) {
+            url += "&question_subject=" + (this.data.subjectIndex + 1);
+        }
+        wx.navigateTo({
+            url: url
+        })
+    }
+})
