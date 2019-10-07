86fd2aab7110ee6427b22d9da5fee23f3145343d
zhouhenglc <zhouhenglc@inspur.com>
2019-10-07 14:28:21 +0800

    修复问题

--- tools/read_xml.py
+++ tools/read_xml.py
@@ -30,9 +30,29 @@ remote_host = "http://127.0.0.1:2400"
 remote_host = "https://wild.gene.ac"
 
 
-Q_TYPE_COMP = re.compile(u"(选择|名词解释|简答题|计算题|论述题)")
+Q_TYPE_COMP = re.compile(u"((一|二|三|四|五)、|^)(单选|选择|名词解释|简答题|计算题|论述题)")
 S_ANSWER_COMP = re.compile(r"(\d+)-(\d+)([a-d]+)", re.I)
 G_SELECT_MODE = [u"无", u"选择", u"名词解释", u"简答题", u"计算题", u"论述题"]
+
+
+def get_select_mode(content):
+    fr = Q_TYPE_COMP.findall(content)
+    if len(fr) != 1:
+        return -1
+    s = fr[0][2]
+    if s in G_SELECT_MODE:
+        return G_SELECT_MODE.index(s)
+    if s in (u"单选", u"单选题"):
+        return 1
+    raise RuntimeError("Bad select mode %s" % s)
+
+
+def replace_special_space(s):
+    for c in (u"\u3000", u"\xa0"):
+        s = s.replace(c, " ")
+    # for c in (u"\u6bb5", ):
+    #     s = s.replace(c, "\n")
+    return s
 
 
 @contextmanager
@@ -62,6 +82,21 @@ def _get_node(p_node, node_name):
     return filter(lambda x: x.nodeName == node_name, p_node.childNodes)
 
 
+def _get_one_node(p_node, node_name):
+    children = _get_node(p_node, node_name)
+    if len(children) != 1:
+        raise RuntimeError("Fond not 1(is %s) %s" % (len(children), node_name))
+    return children[0]
+
+
+def get_deep_node(p_node, node_name):
+    _names = node_name.split(".")
+    p_nodes = [p_node]
+    for name in _names:
+        p_nodes = _get_node(p_nodes[0], name)
+    return p_nodes
+
+
 def analysis_style(style):
     h_comp = re.compile(r"height:([\d.]+?)pt")
     w_comp = re.compile(r"width:([\d.]+?)pt")
@@ -73,10 +108,15 @@ def analysis_style(style):
 def handle_paragraph(p_node):
     run_children = _get_node(p_node, "w:r")
     p_contents = []
+    is_bold = True
     for child in run_children:
         text_children = _get_node(child, "w:t")
         for c in text_children:
             p_contents.append(c.firstChild.nodeValue)
+        # if len(text_children) > 0 and is_bold:
+        #     bold_children = get_deep_node(child, "w:rPr.w:bCs")
+        #     if len(bold_children) <= 0:
+        #         is_bold = False
         object_children = _get_node(child, "w:object")
         for oc in object_children:
             v_shape = _get_node(oc, "v:shape")[0]
@@ -84,6 +124,21 @@ def handle_paragraph(p_node):
             width, height = analysis_style(v_shape_style)
             r_id = _get_node(v_shape, "v:imagedata")[0].getAttribute("r:id")
             p_contents.append("[[%s:%s:%s]]" % (r_id, width, height))
+        # 获得段内换行
+        br_children = _get_node(child, "w:br")
+        if len(br_children) == 1:
+            p_contents.append("\n")
+    if len(p_contents) <= 0:
+        is_bold = False
+    # TODO 返回字符串是否加粗
+    # if is_bold is True and len(p_contents) > 0:
+    #     print("---------------------rpr")
+    #     print("".join(p_contents))
+    # p_bold_children = get_deep_node(p_node, "w:pPr.w:rPr.w:bCs")
+    # if len(p_bold_children) > 0:
+    #     if p_bold_children[0].getAttribute("w:val") != "0":
+    #         print("---------------------ppr")
+    #         print("".join(p_contents))
     return "".join(p_contents)
 
 
@@ -118,17 +173,29 @@ def get_question(question_items, *args):
         return "\n"
     # print(u"(A|B|C|D)(%s)" % "|".join(map(lambda x: re.escape(x), s_c)))
     r_compile = re.compile(u"(^|\s)(A|B|C|D)(%s)" % "|".join(map(lambda x: re.escape(x), s_c)))
+    #  有些选项 A B C D后面没有点
+    rb_compile = re.compile(u"(^|\s)(A|B|C|D)(%s|)" % "|".join(map(lambda x: re.escape(x), s_c)))
+
     for item in question_items[i:]:
-        item = item.replace(u"\u3000", " ")
+        item = replace_special_space(item)
         r_item = r_compile.sub(replace, item)
         options.extend(r_item.split("\n"))
     real_options = map(lambda x: x.strip(), options)
     real_options = filter(lambda x: len(x) > 0, real_options)
+    if len(real_options) < 4:
+        #  有些选项 A B C D后面没有点
+        options_s = "\n".join(question_items[i:])
+        r_options_s = rb_compile.sub(replace, options_s)
+        real_options = r_options_s.split("\n")
+        real_options = map(lambda x: x.strip(), real_options)
+        real_options = filter(lambda x: len(x) > 0, real_options)
     if len(real_options) != 4:
         for r_o in real_options:
             print(r_o)
         print(q_no)
-        print(question_items)
+        for item in question_items:
+            print(item)
+        raise RuntimeError("")
         return None
     # for r_o in real_options:
     #     print(r_o)
@@ -169,12 +236,13 @@ def handle_docx_main_xml(xml_path, *args):
         if node.nodeName != "w:p":
             continue
         p_content = handle_paragraph(node).strip()
+
         if len(p_content) <= 0:
             continue
         # 判断是否是题目类型
-        type_mr = Q_TYPE_COMP.findall(p_content)
-        if len(type_mr) == 1:
-            current_q_type = G_SELECT_MODE.index(type_mr[0])
+        _q_tpe = get_select_mode(p_content)
+        if _q_tpe > 0:
+            current_q_type = _q_tpe
             continue
         # 判断是否是题目
         m_no = m_compile.match(p_content)
@@ -234,12 +302,17 @@ def get_qa_answers(answer_items):
         # 判断是否是答案开始
         found_items = qa_aw_comp.findall(item)
         if found_items:
+            found_item = found_items[0]
+            next_no = int(found_item[0])
+            if next_no < current_no:
+                # 不允许出现同一个答案区域，出现题号下降。防止答案里出现小题题号，出现误判
+                current_answer += "\n" + item
+                continue
             if current_no != -1:
                 if current_no in aw_dict:
                     raise RuntimeError("repeated answers %s" % current_no)
                 aw_dict[current_no] = current_answer
-            found_item = found_items[0]
-            current_no = int(found_item[0])
+            current_no = next_no
             current_answer = found_item[2]
         else:
             current_answer += "\n" + item
@@ -275,12 +348,14 @@ def handle_answers_docx_main_xml(xml_path):
         if node.nodeName != "w:p":
             continue
         p_content = handle_paragraph(node).strip()
+
         if len(p_content) <= 0:
             continue
-        type_mr = Q_TYPE_COMP.findall(p_content)
-        if len(type_mr) == 1:
+        _q_type = get_select_mode(p_content)
+        if _q_type >= 0:
+            # match到关键字 且字符串长度不能
             _get_answers()
-            current_q_type = G_SELECT_MODE.index(type_mr[0])
+            current_q_type = _q_type
             current_answers_area = []
             continue
         current_answers_area.append(p_content)
@@ -344,44 +419,46 @@ def replace_media(text, q_rl, cache_rl):
         text = text.replace(r_t, "[[%s:%s:%s]]" % (r_url, width, height))
     return text
 
-from paste import urlmap
+
 def handle_exam(file_path):
     exam_no = 1567506833
     no_info = req_max_no(exam_no)
     next_no = no_info["next_no"]
 
-    try:
-        uploaded_aw_rl = dict()
-        uploaded_q_rl = dict()
-        exam_name = os.path.basename(file_path).rsplit(".", 1)[0]
-        answer_file = file_path.replace(".docx", u"答案.docx")
-        with read_docx(file_path) as rd, read_answers_docx(answer_file) as rad:
-            question_list, q_rl = rd
-            answers_dict, aw_rl = rad
-            for q_item in question_list:
-                q_no = q_item["no"]
-                # 判定是否包含答案
-                if q_no not in answers_dict:
-                    raise RuntimeError("lack answer %s" % q_item["no"])
-                if q_item["select_mode"] == 1:  # 选择题
-                    q_item["options"][answers_dict[q_no]]["score"] = 1
-                    q_item["answer"] = ""
-                else:
-                    q_item["answer"] = answers_dict[q_no]
-                # 开始上传 题目
-                # 获取题目描述中的图片
-                q_item["desc"] = replace_media(q_item["desc"], q_rl, uploaded_q_rl)
+    uploaded_aw_rl = dict()
+    uploaded_q_rl = dict()
+    exam_name = os.path.basename(file_path).rsplit(".", 1)[0]
+    answer_file = file_path.replace(".docx", u"答案.docx")
+    if os.path.exists(answer_file) is False:
+        msg = ("Ignore %s, not Answer" % file_path)
+        return False, msg
+    print("start handle %s" % exam_name)
 
-                # 获取选项中的图片
-                for option in q_item["options"]:
-                    option["desc"] = replace_media(option["desc"], q_rl, uploaded_q_rl)
-                # 获取答案中的图片
-                q_item["answer"] = replace_media(q_item["answer"], aw_rl, uploaded_aw_rl)
-            post_questions(exam_name, exam_no, next_no, question_list)
-    except Exception as e:
-        traceback.print_exc()
-        print(e)
-        return False, str(e)
+    with read_docx(file_path) as rd, read_answers_docx(answer_file) as rad:
+        question_list, q_rl = rd
+        answers_dict, aw_rl = rad
+        for q_item in question_list:
+            q_no = q_item["no"]
+            # 判定是否包含答案
+            if q_no not in answers_dict:
+                pdb.set_trace()
+                print(exam_name)
+                raise RuntimeError("lack answer %s" % q_item["no"])
+            if q_item["select_mode"] == 1:  # 选择题
+                q_item["options"][answers_dict[q_no]]["score"] = 1
+                q_item["answer"] = ""
+            else:
+                q_item["answer"] = answers_dict[q_no]
+            # 开始上传 题目
+            # 获取题目描述中的图片
+            q_item["desc"] = replace_media(q_item["desc"], q_rl, uploaded_q_rl)
+
+            # 获取选项中的图片
+            for option in q_item["options"]:
+                option["desc"] = replace_media(option["desc"], q_rl, uploaded_q_rl)
+            # 获取答案中的图片
+            q_item["answer"] = replace_media(q_item["answer"], aw_rl, uploaded_aw_rl)
+        post_questions(exam_name, exam_no, next_no, question_list)
     return True, "success"
 
 
@@ -410,6 +487,7 @@ def find_from_dir(directory_name):
             with open("error.text", "w") as we:
                 we.write(file_path)
                 we.write(msg)
+            print(msg)
         # if len(members) <= 0:
         #     print(u"请检查文件%s" % file_path)
         # all_member.extend(members)
