8b1613656a74326994ae11a8082b9e942f26c538
zhouhenglc <zhouhenglc@inspur.com>
2023-07-27 11:03:05 +0800

    add qian

--- wildzh/utils/datetime_helper.py
+++ wildzh/utils/datetime_helper.py
@@ -33,3 +33,41 @@ def calc_day_period_no(t=None):
         t = time.time()
     period_no = (t - BASIC_PT) / ONE_DAY_PERIOD
     return int(period_no)
+
+
+def get_month_days(year, month):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    if month in (4, 6, 9, 11):
+        return 30
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return 29
+        return 28
+    elif year % 4 == 0:
+        return 29
+    return 28
+
+
+def calc_ymd(dt1, dt2):
+    v1 = '%s%02d%02d%02d%02d%02d' % (dt1.year, dt1.month, dt1.day, dt1.hour,
+                                     dt1.minute, dt1.second)
+    v2 = '%s%02d%02d%02d%02d%02d' % (dt2.year, dt2.month, dt2.day, dt2.hour,
+                                     dt2.minute, dt2.second)
+    v1 = int(v1)
+    v2 = int(v2)
+    offset = int((v1 - v2) / 1000000)
+    year = int(offset / 10000)
+    offset %= 10000
+    month = int(offset / 100)
+    offset %= 100
+    if month > 12:
+        month = (month + 12) % 100
+    day = offset
+    if day > 31:
+        day = (day + get_month_days(dt2.year, dt2.month)) % 100
+    interval = {'year': year, 'month': month, 'day': day}
+    delta = dt1 - dt2
+    dur = {'total_days': delta.days, 'total_seconds': delta.total_seconds()}
+    dur['interval'] = interval
+    return dur
--- wildzh/web02/views/index_view.py
+++ wildzh/web02/views/index_view.py
@@ -5,6 +5,7 @@ from flask import g
 from flask import request
 from flask_helper.template import RenderTemplate
 
+from wildzh.utils import datetime_helper
 from wildzh.utils.log import getLogger
 from wildzh.classes.version import VersionMP
 from wildzh.web02.view import View2
@@ -36,3 +37,11 @@ def report_wx_min_program_version():
     LOG.info('Receive user(user_no=%s) report wx min_program version: %s',
              g.user_no, version)
     return jsonify({'status': r, 'data': data})
+
+
+@index_view.route('/qian', methods=['GET'])
+def qian_page():
+    from datetime import datetime
+    dt = datetime(2023, 3, 31, 11, 18)
+    data = datetime_helper.calc_ymd(datetime.now(), dt)
+    return rt.render('qian.html', data=data)
